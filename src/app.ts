38a8a695deecbf19463ed60ebb7a603918a1bc3d
debasishch <139261862+debasishch@users.noreply.github.com>
2024-09-21 22:51:19 +0530

    create user router

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,19 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import createHttpError, { HttpError } from "http-errors";
 import { config } from "./config/config";
 import globalErrorHandler from "./middleware/globalErrorHandler";
+import userRouter from "./user/userRouter";
 
 const app = express();
 
 app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to elib apis" });
 });
-//Global Error Handler
+//middlewares
+app.use('/api/users', userRouter);
 
+
+//Global Error Handler
 app.use(globalErrorHandler);
 
 export default app;
