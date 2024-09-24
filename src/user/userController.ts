19260af825bdf91c5c48ca200ede44043292af4a
debasishch <139261862+debasishch@users.noreply.github.com>
2024-09-24 20:08:16 +0530

    add user model

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
+import { error } from "console";
 
 const createUser = async ( req: Request,res: Response,next: NextFunction)=>{
 
@@ -7,11 +9,21 @@ const createUser = async ( req: Request,res: Response,next: NextFunction)=>{
 
     //validation
     if (!name || !email || !password){
-        
+
         const error= createHttpError(400,"All fiels are required" )
     
         return next(error);
     }
+
+    //Databse call
+    const user=await userModel.findOne({email});
+    if (user){
+        const error = createHttpError(400,"User already exists with this email.")
+    }   return next(error)
+
+
+    //Process
+    //Response
     res.json({
         "message": "User Created"
     })
