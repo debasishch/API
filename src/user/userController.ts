b2ba739c19de822963058ea7bb928a6ee9c14dd2
debasishch <139261862+debasishch@users.noreply.github.com>
2024-09-24 20:49:25 +0530

    add hash function

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,36 +1,35 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModel from "./userModel";
+import bcrypt from "bcrypt";
 import { error } from "console";
 
-const createUser = async ( req: Request,res: Response,next: NextFunction)=>{
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { name, email, password } = req.body;
 
-    const {name, email, password}=req.body;
+  //validation
+  if (!name || !email || !password) {
+    const error = createHttpError(400, "All fiels are required");
 
-    //validation
-    if (!name || !email || !password){
+    return next(error);
+  }
 
-        const error= createHttpError(400,"All fiels are required" )
-    
-        return next(error);
-    }
-
-    //Databse call
-    const user=await userModel.findOne({email});
-    if (user){
-        const error = createHttpError(400,"User already exists with this email.")
-    }   return next(error)
+  //Databse call
+  const user = await userModel.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email.");
+    return next(error);
+  }
+  //password-hash
+  const hashedPassword = await bcrypt.hash(password,10)
 
 
-    //Process
-    //Response
-    res.json({
-        "message": "User Created"
-    })
+
+  //Process
+  //Response
+  res.json({
+    message: "User Created",
+  });
 };
 
-
-export {createUser};
-
-
-
+export { createUser };
