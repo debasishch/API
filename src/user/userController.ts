6fab478d3bce96e4460e8fc1213201b3a3a892c4
debasishch <139261862+debasishch@users.noreply.github.com>
2024-09-25 21:32:19 +0530

    add error handle

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -5,6 +5,7 @@ import bcrypt from "bcryptjs";
 import { error } from "console";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -17,29 +18,45 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   //Databse call
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User already exists with this email.");
-    return next(error);
+  try {
+    
+    const user = await userModel.findOne({ email });
+
+    if (user) {
+      const error = createHttpError(400, "User already exists with this email.");
+      return next(error);
   }
+ } catch (err) {
+    return next(createHttpError(500,'error while getting user'))
+  }
+  
   //password-hash
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser= await userModel.create({
+  let newUser: User;
+try{
+  newUser= await userModel.create({
     name,
     email,
     password: hashedPassword
   });
 
+}catch(err){
+  return next(createHttpError(500,"Error while creating user"))
+}
+try {
+  
   //Token Generation-JWT
   const token = sign({sub: newUser._id}, config.jwtSecret as string,
      {expiresIn: "7d",})
 
-  //Process
   //Response
   res.json({ 
     accessToken: token
   });
+} catch (err) {
+  return next(createHttpError(500, "Error while signing the jwt token"))
+}
 };
 
 export { createUser };
